7f71f6b94303a28ceb628619154e5aa587d0f433
Erin Moon <erin@hashbang.sh>
2018-11-27 13:06:56 -0600

    start migration to androidx

--- app/src/androidTest/java/rs/hecke/delta7/mamimiapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/rs/hecke/delta7/mamimiapp/ExampleInstrumentedTest.kt
@@ -1,7 +1,7 @@
 package rs.hecke.delta7.mamimiapp
 
-import android.support.test.InstrumentationRegistry
-import android.support.test.runner.AndroidJUnit4
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
 
 import org.junit.Test
 import org.junit.runner.RunWith
