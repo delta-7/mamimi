253de6e6c03af7a5313c80253049e3ac3050cf14
Erin Moon <erin@hashbang.sh>
2018-11-27 14:55:16 -0600

    mamimi: start working on RecordButton

--- mamimi/src/main/java/rs/hecke/delta7/mamimi/widgets/RecordButton.kt
+++ mamimi/src/main/java/rs/hecke/delta7/mamimi/widgets/RecordButton.kt
@@ -1,1 +1,13 @@
 package rs.hecke.delta7.mamimi.widgets
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.ImageButton
+
+class RecordButton @JvmOverloads constructor(
+    ctx: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : ImageButton(ctx, attrs, defStyleAttr) {
+    init {
+        setBackgroundResource(0); // remove the background
+    }
+}
