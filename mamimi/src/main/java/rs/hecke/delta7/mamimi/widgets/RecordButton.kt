71f37dc3a2839620bb166d59d8bc8c3aa134581a
Erin Moon <erin@hashbang.sh>
2018-11-28 09:19:52 -0600

    recordbutton: draw something

--- mamimi/src/main/java/rs/hecke/delta7/mamimi/widgets/RecordButton.kt
+++ mamimi/src/main/java/rs/hecke/delta7/mamimi/widgets/RecordButton.kt
@@ -1,5 +1,7 @@
 package rs.hecke.delta7.mamimi.widgets
 
+import rs.hecke.delta7.mamimi.R;
+
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.ImageButton
@@ -9,5 +11,7 @@ class RecordButton @JvmOverloads constructor(
 ) : ImageButton(ctx, attrs, defStyleAttr) {
     init {
         setBackgroundResource(0); // remove the background
+        setImageResource(R.drawable.take_picture_button);
+        setPadding(10, 10, 10,10);
     }
 }
